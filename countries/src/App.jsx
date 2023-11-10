308de0284964c5bfa6249a4146e0f311cb131c25
Eva Doncieux <eva.doncieux@gmail.com>
2023-11-10 15:54:59 +0100

    implementing routing

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -2,29 +2,52 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import fetchCountries from "../services/fetchServices.js";
 import { Nav } from "./components/Nav.jsx";
-import { Card } from "./components/Card.jsx";
 import { Detail } from "./components/Detail.jsx";
+import { Filter } from "./components/Filter.jsx";
+import Card from "./components/Card.jsx";
+import Search from "./components/Search.jsx";
 
 function App() {
-  const [countries, setCountries] = useState([]);
+    const [countries, setCountries] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const countriesData = await fetchCountries();
-                setCountries(countriesData);
+                console.log("Fetched countries data:", countriesData);
+    
+                if (Array.isArray(countriesData)) {
+                    setCountries(countriesData);
+                    setLoading(false);
+                } else {
+                    console.error("Invalid countries data:", countriesData);
+                    setLoading(false);
+                }
             } catch (error) {
                 console.error("Error fetching data", error);
+                setLoading(false);
             }
         };
         fetchData();
     }, []);
+    
 
     return (
         <div>
             <Nav />
-            {/* <Card countries={countries} /> */}
-            <Detail countries={countries} />
+            <div className="filter-search">
+                <Filter countries={countries} />
+                <Search countries={countries} />
+            </div>
+            {loading ? (
+                <div>Loading... ^^</div>
+            ) : countries ? (
+                <Card countries={countries} />
+            ) : (
+                <div>No countries data available.</div>
+            )}
+            {/* <Detail countries={countries} /> */}
         </div>
     );
 }
